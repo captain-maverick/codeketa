7f2fbce89ffbce48b458a86c89c5490b9803ebbd
captain-maverick <52990483+captain-maverick@users.noreply.github.com>
2019-07-17 10:57:25 +0530

    Create prgm.c

---
+++ prgm.c
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	scanf("%d",&n);
+	if(n>0)
+	printf("positive");
+	else if (n<0)
+	printf("negative");
+	else if(n==0)
+	printf("zero");
+	return 0;
+}
